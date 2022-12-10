d959e028b8e31d03f3438202d8cc8545f7ae32f7
froggy1014 <clcl6084@gmail.com>
2022-12-10 23:24:14 +0900

    feat[index.js]: QueryClient Option added
    1. refetchOnWindowFous, IntervalInBackground - false
    2. keepPreviouseDate - true
    3. statkeTime: Infinity - for develpment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ import MainHeader from './shared/components/Navigation/MainHeader';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      refetchOnWindowFocus: false,
+      refetchIntervalInBackground: false,
+      keepPreviousData: true,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
