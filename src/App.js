83d757a72fbcb5cf8dcdfd7b3ea7fcb6eee7bc00
froggy1014 <clcl6084@gmail.com>
2022-12-16 01:23:57 +0900

    feat: create SuspenseWrapper for code spliting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import React, { useState, useCallback } from 'react';
-import Users from './user/pages/Users';
-import UserPlaces from './places/pages/UserPlaces';
-import UpdatePlace from './places/pages/UpdatePlace';
-import NewPlaces from './places/pages/NewPlaces';
-import Authenticate from './user/pages/Authenticate';
+
 import {useAuth} from './shared/hooks/auth-hook'
 
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import { AppContext } from './shared/context/AppContext'
 import ErrorModal from './shared/components/UIElements/ErrorModal';
+import LoadingSpinner from 'shared/components/UIElements/LoadingSpinner';
+import SuspenseWrapper from 'shared/components/Navigation/SuspenseWrapper'
+
+// import Users from './user/pages/Users';
+// import UserPlaces from './places/pages/UserPlaces';
+// import UpdatePlace from './places/pages/UpdatePlace';
+// import NewPlaces from './places/pages/NewPlaces';
+// import Authenticate from './user/pages/Authenticate';
+
 
 function App() {
   const [modalShow, setModalShow] = useState(false);
@@ -27,19 +32,19 @@ function App() {
   if(token) {
     routes = (
       <React.Fragment>
-        <Route exact path="/" element={<Users />} />
-        <Route exact path="/:userId/places" element={<UserPlaces />}/>
-        <Route exact path="/places/new" element={<NewPlaces />} />
-        <Route path="/places/:placeId" element={<UpdatePlace />} />
+        <Route exact path="/" element={<SuspenseWrapper path={'user/pages/Users'} />} />
+        <Route exact path="/:userId/places" element={<SuspenseWrapper path={'places/pages/UserPlaces'} />}/>
+        <Route exact path="/places/new" element={<SuspenseWrapper path={'places/pages/NewPlaces'} />} />
+        <Route path="/places/:placeId" element={<SuspenseWrapper path={'places/pages/UpdatePlace'} />} />
         <Route path="*" element={<Navigate to="/" />} />
       </React.Fragment>
     );
   } else {
     routes = (
       <React.Fragment>
-        <Route exact path="/" element={<Users />} />
-        <Route exact path="/:userId/places" element={<UserPlaces />}/>
-        <Route path="/auth" element={<Authenticate />} />
+        <Route exact path="/" element={<SuspenseWrapper path={'user/pages/Users'} />} />
+        <Route exact path="/:userId/places" element={<SuspenseWrapper path={'places/pages/UserPlaces'} />}/>
+        <Route path="/auth" element={<SuspenseWrapper path={'user/pages/Authenticate'} />} />
         <Route path="*" element={<Navigate to="/auth" />} />
       </React.Fragment>
     );
